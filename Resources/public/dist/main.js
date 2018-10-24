2b3624bed845bf50690e004e3bfb5cb051e863f7
Weiye Sun <weiye.sun@gmail.com>
2018-10-24 22:50:13 +0200

    Enable some plugins for ckeditor

--- Resources/public/dist/main.js
+++ Resources/public/dist/main.js
@@ -9,15 +9,27 @@ define(function() {
     return {
         name: "Base Application Bundle",
         initialize: function(app) {
-            app.sandbox.ckeditor.addPlugin('colorbutton');
-            app.sandbox.ckeditor.addToolbarButton('colors', 'TextColor', 'cog');
-            app.sandbox.ckeditor.addToolbarButton('colors', 'BGColor', 'cogs');
-            // app.sandbox.ckeditor.addToolbarButton('insert', 'Image', 'picture-o');
-            app.sandbox.ckeditor.addToolbarButton('insert', 'HorizontalRule', 'arrows-h');
-            app.sandbox.ckeditor.addToolbarButton('semantics', 'Styles', 'css3');
-            app.sandbox.ckeditor.addToolbarButton('basicstyles', 'RemoveFormat', 'filter');
-            app.sandbox.ckeditor.addToolbarButton('paste', 'PasteText', 'file-text');
-            app.sandbox.ckeditor.addToolbarButton('paragraph', 'Blockquote', 'quote-right');
+            let ckeditor = app.sandbox.ckeditor,
+                toolbar = ckeditor.getToolbar();
+
+            CKEDITOR.skinName = 'kama';
+
+            ckeditor.addPlugin('colorbutton');
+            ckeditor.addPlugin('image2');
+            ckeditor.addPlugin('selectall');
+            ckeditor.addPlugin('find');
+            ckeditor.addPlugin('font');
+            ckeditor.addPlugin('clipboard');
+
+            toolbar.basicstyles = ['FontSize', ...toolbar.basicstyles, 'TextColor', 'BGColor', 'Smiley'];
+            toolbar.insert = [...toolbar.insert, 'Image', 'HorizontalRule', 'MagicLine'];
+            toolbar.semantics.push('Styles');
+            toolbar.paste.push('PasteText');
+            toolbar.edition = [ 'Find', 'Replace', 'SelectAll', 'RemoveFormat', 'Undo', 'Redo' ];
+            toolbar.paragraph = ['Blockquote'];
+            toolbar.view = ['Maximize'];
+
+            ckeditor.setToolbar({...toolbar});
         }
     };
 });
